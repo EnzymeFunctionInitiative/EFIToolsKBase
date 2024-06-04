78e10606733564b99f26e1ca9818a5af8b8d386f
Hunter DeMeyer <demeyer3@illinois.edu>
2024-06-04 16:09:09 -0500

    Remove report objects

--- lib/EFIToolsKBase/EFIToolsKBaseImpl.py
+++ lib/EFIToolsKBase/EFIToolsKBaseImpl.py
@@ -92,10 +92,11 @@ class EFIToolsKBase:
         self.flow.render_params_file("/results/sequences.fasta")#params['fasta_sequences_file'])
         self.flow.generate_run_command()
         retcode, stdout, stderr = self.flow.execute()
-        report_data = {
-            'objects_created': [{}],
-                'text_message': "message"
-        }
+        report_data = {}
+        #     "retcode": retcode,
+        #     "stdout": stdout,
+        #     "stderr": stderr,
+        # }
         kbase_report = KBaseReport(self.callback_url)
         report = kbase_report.create({
             'report': report_data,
