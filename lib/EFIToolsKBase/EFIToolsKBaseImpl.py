d7504f0fa027368b44e937b93ef85e546705bea8
Hunter DeMeyer <demeyer3@illinois.edu>
2024-06-13 12:16:21 -0500

    Rewrite file path param after fetch

--- lib/EFIToolsKBase/EFIToolsKBaseImpl.py
+++ lib/EFIToolsKBase/EFIToolsKBaseImpl.py
@@ -96,7 +96,8 @@ class EFIToolsKBase:
            params["fasta_sequences_file"] = "/results/sequences.fasta"
         else:
             download_staging_file_params = {'staging_file_subdir_path': params["fasta_sequences_file"]}
-            efi.dfu.download_staging_file(download_staging_file_params)
+            retval = efi.dfu.download_staging_file(download_staging_file_params)
+            params["fasta_sequences_file"] = retval["copy_file_path"]
         output = efi.est_fasta(params)
         
         #END run_EFI_EST_FASTA
