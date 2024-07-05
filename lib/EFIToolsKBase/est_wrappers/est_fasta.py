7923ef6c22f9ed873601c51985a2f422dc300334
Hunter DeMeyer <demeyer3@illinois.edu>
2024-07-05 15:41:01 -0500

    Use new FASTA import functionality in pipeline

--- lib/EFIToolsKBase/est_wrappers/est_fasta.py
+++ lib/EFIToolsKBase/est_wrappers/est_fasta.py
@@ -7,11 +7,19 @@ class EFIFasta(EFIEST):
         seq_obj = self.dfu.get_objects({"object_refs": [params["fasta_sequences_file"]]})["data"][0]
         sequences = seq_obj["data"]["sequences"]
         uploaded_fasta_file = os.path.join(self.shared_folder, "input_sequences.fasta")
+        seqs_written = 0
         with open(uploaded_fasta_file, "w") as f:
             for record in sequences:
                 id = record["id"]
                 sequence = record["sequence"]
-                f.writelines([f"{id}\n", f"{sequence}\n"])
+                f.write(f">{id}")
+                f.write("\n")
+                f.write(sequence)
+                f.write("\n")
+                seqs_written += 1
+        print(f"Wrote {seqs_written} sequences")
+        # with open(uploaded_fasta_file) as f:
+        #     print(f.readlines())
         mapping = {
             "final_output_dir": self.shared_folder,
             "duckdb_memory_limit": "64GB",
@@ -23,11 +31,10 @@ class EFIFasta(EFIEST):
             "efi_config": "/EST/efi.config",
             "fasta_db": "/data/blastdb/combined.fasta",
             "efi_db": "/data/efi_db.sqlite",
-            "import_mode": "FASTA",
-            "exclude_fragments": True if params["fragment_option"] == 1 else False,
+            "import_mode": "fasta",
             "multiplex": False,
             "job_id": 131,
-
-            "fasta_file": uploaded_fasta_file
+            "blast_evalue": "1e-5",
+            "uploaded_fasta_file": uploaded_fasta_file
         }
         return self.run_est_pipeline(mapping, params["workspace_name"])
