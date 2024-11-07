c2a397dfc1d60182564c5c86d6f7e23573b1a207
Nils Oberg <noberg@illinois.edu>
2024-11-07 11:10:59 -0600

    Rename variables for clarity

--- scripts/download_file.py
+++ scripts/download_file.py
@@ -35,13 +35,13 @@ def parse_args() -> argparse.Namespace:
     return args
 
 
-def get_remote_file_contents(remote_file: str) -> str:
+def get_remote_file_contents(remote_file_url: str) -> str:
     """
     Download the file as a string and return file contents to user.
 
     Parameters
     ----------
-        remote_file
+        remote_file_url
             complete URL for a file
 
     Returns
@@ -50,7 +50,7 @@ def get_remote_file_contents(remote_file: str) -> str:
     """
 
     try:
-        response = urllib.request.urlopen(remote_file)
+        response = urllib.request.urlopen(remote_file_url)
     except urllib.error.HTTPError as e:
         return None
 
@@ -58,10 +58,10 @@ def get_remote_file_contents(remote_file: str) -> str:
     return io.StringIO(str(file_contents.decode('utf-8')))
 
 
-def get_file_list(remote_dir: str, remote_file: str) -> dict:
+def get_file_list(remote_dir_url: str, remote_file: str) -> dict:
     """
     Retrieve a list of files created by 'split' on the remote that need to be fetched.
-    If a file 'remote_dir/remote_file.file_list' is present on the server, that means
+    If a file 'remote_dir_url/remote_file.file_list' is present on the server, that means
     that the file has been split and a list of the split files is returned.  If there
     is no file ending in '.file_list' then an empty list is returned.  '.file_list'
     is assumed to be the output of the 'md5sum' program used to generate hashes for
@@ -69,7 +69,7 @@ def get_file_list(remote_dir: str, remote_file: str) -> dict:
 
     Parameters
     ----------
-        remote_dir
+        remote_dir_url
             URL of directory on remote that contains files
         remote_file
             name of the file on the remote to download
@@ -80,9 +80,9 @@ def get_file_list(remote_dir: str, remote_file: str) -> dict:
             dictionary of files to checksum (MD5) for each file on the remote
     """
 
-    remote_file = remote_dir + "/" + remote_file + ".file_list"
+    remote_file_url = remote_dir_url + "/" + remote_file + ".file_list"
 
-    remote_listing = get_remote_file_contents(remote_file)
+    remote_listing = get_remote_file_contents(remote_file_url)
     if remote_listing is None:
         return None
 
@@ -94,12 +94,12 @@ def get_file_list(remote_dir: str, remote_file: str) -> dict:
     return files
 
 
-def get_remote_checksum(remote_dir: str, remote_file: str) -> str:
+def get_remote_checksum(remote_dir_url: str, remote_file: str) -> str:
     """
     Return the MD5 hash from the MD5 file corresponding to the remote file (not split).
 
     Parameters:
-        remote_dir
+        remote_dir_url
             URL of directory on remote that contains files
         remote_file
             name of the file on the remote to download
@@ -108,9 +108,10 @@ def get_remote_checksum(remote_dir: str, remote_file: str) -> str:
     -------
         MD5 hash of the remote file as provided by the remote
     """
-    remote_file = remote_dir + "/" + remote_file + ".md5"
 
-    checksum_data = get_remote_file_contents(remote_file)
+    remote_file_url = remote_dir_url + "/" + remote_file + ".md5"
+
+    checksum_data = get_remote_file_contents(remote_file_url)
     if checksum_data is None:
         return None
 
@@ -182,6 +183,7 @@ def compare_md5(remote_md5: str, local_md5: str) -> str:
         local_md5
             hash computed locally of local file by md5sum 
     """
+
     if remote_md5 != local_md5:
         print(f"Remote MD5 {remote_md5} doesn't match local MD5 {local_md5}")
         return False
