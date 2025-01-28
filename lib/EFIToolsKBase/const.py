263a58ba06ed41a78f5b81217afd2521af53e568
Russell Davidson <rbdavid@illinois.edu>
2025-01-28 16:29:43 -0600

    update input parameter dictionary for the est.nf code

--- lib/EFIToolsKBase/const.py
+++ lib/EFIToolsKBase/const.py
@@ -5,4 +5,5 @@ TEMPLATES_DIR = os.path.join(MODULE_DIR, "lib/templates")
 
 EFI_CONFIG_PATH = "/EST/efi.config"
 EFI_DB_PATH = "/data/efi_db.sqlite"
+FASTA_DB_PATH = "/data/blastdb/combined.fasta"
 EFI_DB_VERSION = 101
