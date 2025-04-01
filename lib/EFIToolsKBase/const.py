52eaba53bef0169939e2aab90dfbc39e6e8bfa5e
Russell Davidson <rbdavid@illinois.edu>
2025-04-01 12:01:57 -0500

    update the hard-coded efi-db version

--- lib/EFIToolsKBase/const.py
+++ lib/EFIToolsKBase/const.py
@@ -6,4 +6,4 @@ TEMPLATES_DIR = os.path.join(MODULE_DIR, "lib/templates")
 EFI_CONFIG_PATH = "/EST/efi.config"
 EFI_DB_PATH = "/data/efi_db.sqlite"
 FASTA_DB_PATH = "/data/blastdb/combined.fasta"
-EFI_DB_VERSION = 101
+EFI_DB_VERSION = 104
