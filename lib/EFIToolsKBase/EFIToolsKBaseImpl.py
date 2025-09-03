af3cfb4ccbbfc795332efe01b95afa0998cb6ce8
Russell Davidson <rbdavid@illinois.edu>
2025-09-03 14:11:57 -0500

    update version numbers throughout spec.json files to match the EFIToolsKBase version number on which the apps were last affected

--- lib/EFIToolsKBase/EFIToolsKBaseImpl.py
+++ lib/EFIToolsKBase/EFIToolsKBaseImpl.py
@@ -37,7 +37,7 @@ class EFIToolsKBase:
     # state. A method could easily clobber the state set by another while
     # the latter method is running.
     ######################################### noqa
-    VERSION = "0.4.3"
+    VERSION = "0.4.4"
     GIT_URL = "git@github.com:EnzymeFunctionInitiative/EFIToolsKBase.git"
     GIT_COMMIT_HASH = "4777308f04a169666b063f3eb576263da162c9ae"
 
